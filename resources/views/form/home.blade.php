e036463816012b164117a3502cf45ea57236a440
mike <mike@hipstercreative.com>
2017-02-03 10:04:10 -0800

    Return Auth user to blade

--- resources/views/form/home.blade.php
+++ resources/views/form/home.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Form</title>
-        <link rel="stylesheet" href="../css/app.css">
-    </head>
-    <body>
-        <div id="app">
-            <form-app></form-app>
-        </div>
-    </body>
-    <script src="../js/app.js"></script>
-</html>
+@section('content')
+
+@endsection
