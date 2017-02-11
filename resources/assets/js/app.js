99a5c44dfb7669c016c8ce298b097e0f08ea850c
mike <mike@hipstercreative.com>
2017-02-10 16:01:22 -0800

    Clean up router and components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,29 @@
 require('./bootstrap');
 
-import router from './router'
-import components from './components'
+import App from './components/App.vue'
+import Nav from './components/Nav.vue'
+import Form from './components/Form.vue'
+import Login from './components/auth/Login.vue'
+import FormEdit from './components/FormEdit.vue'
+import Register from './components/auth/Register.vue'
 
-for(var component in components) {
-    Vue.component(component, components[component])
-}
+Vue.component('form-app', App, { name: 'form-app' })
+Vue.component('form-nav', Nav, { name: 'form-nav' })
+Vue.component('form-form', Form, { name: 'form-form' })
+Vue.component('form-login', Login, { name: 'form-login' })
+Vue.component('form-edit', FormEdit, { name: 'form-edit' })
+Vue.component('form-register', Register, { name: 'form-register' })
 
-const app = new Vue({
+const router = new VueRouter({
+    // mode: 'history',
+    routes: [
+        { path: '/', component: Form, name: 'form' },
+        { path: '/login', component: Login, name: 'auth.login' },
+        { path: '/:id/edit', component: FormEdit, name: 'form.edit' },
+        { path: '/register', component: Register, name: 'auth.register' }
+    ]
+})
+
+new Vue({
     router,
-    render: h => h(components['form-app']),
-    components: components,
-}).$mount('#app');
+}).$mount('#spapp');
