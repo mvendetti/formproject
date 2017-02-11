ba39a49ea90318a58935ee393534038c78690bd8
mike <mike@hipstercreative.com>
2017-02-10 16:03:11 -0800

    If user null

--- resources/assets/js/components/Form.vue
+++ resources/assets/js/components/Form.vue
@@ -1,49 +1,54 @@
 <template>
     <div>
-        <div class="row">
-            <div class="col-md-3 col-md-offset-3">
-                <form @submit.prevent="onSubmit">
-                    <h1>files discovered</h1>
-                    <div :class="['form-group', errorFile ? 'has-error' : '']">
-                        <select v-model="selected" class="form-control" size="5" required>
-                            <option v-for="file in files">
-                                {{ file }}
-                            </option>
-                            <option v-if="files.length == 0" disabled>no new videos</option>
-                        </select>
-                        <p v-for="error in errors.file_name" class="error-message">{{ error }}</p>
-                    </div>
+        <div v-if="this.user === null">
+            <div class="row">
+                <div class="col-md-3 col-md-offset-3">
+                    <form @submit.prevent="onSubmit">
+                        <h1>files discovered</h1>
+                        <div :class="['form-group', errorFile ? 'has-error' : '']">
+                            <select v-model="selected" class="form-control" size="5" required>
+                                <option v-for="file in files">
+                                    {{ file }}
+                                </option>
+                                <option v-if="files.length == 0" disabled>no new videos</option>
+                            </select>
+                            <p v-for="error in errors.file_name" class="error-message">{{ error }}</p>
+                        </div>
 
-                    <h1>file information</h1>
-                    <div :class="['form-group', errorOwner ? 'has-error' : '']">
-                        <label>owner name:</label>
-                        <input v-model="ownerName" class="form-control" type="text" required>
-                        <p v-for="error in errors.owner_name" class="error-message">{{ error }}</p>
-                    </div>
-                    <div :class="['form-group', errorDescription ? 'has-error' : '']">
-                        <label>description:</label>
-                        <textarea v-model="description" class="form-control" rows="7" required></textarea>
-                        <p v-for="error in errors.description" class="error-message">{{ error }}</p>
-                    </div>
-                    <button type="submit" class="btn btn-lg btn-default pull-right">Submit</button>
-                </form>
-            </div>
+                        <h1>file information</h1>
+                        <div :class="['form-group', errorOwner ? 'has-error' : '']">
+                            <label>owner name:</label>
+                            <input v-model="ownerName" class="form-control" type="text" required>
+                            <p v-for="error in errors.owner_name" class="error-message">{{ error }}</p>
+                        </div>
+                        <div :class="['form-group', errorDescription ? 'has-error' : '']">
+                            <label>description:</label>
+                            <textarea v-model="description" class="form-control" rows="7" required></textarea>
+                            <p v-for="error in errors.description" class="error-message">{{ error }}</p>
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-default pull-right">Submit</button>
+                    </form>
+                </div>
 
-            <div class="col-md-3">
-                <h1>files submitted</h1>
-                <form>
-                    <div class="form-group">
-                        <select v-model="submittedSelectedId" class="form-control" size="7" required>
-                            <option v-for="file in submittedFiles" :value="file.id">
-                                {{ file.file_name }}
-                            </option>
-                            <option v-if="submittedFiles.length == 0" disabled>no videos submitted</option>
-                        </select>
-                    </div>
-                    <router-link :to="{ name: 'form.edit', params: { id : this.submittedSelectedId } }" class="btn btn-lg btn-primary">Edit</router-link>
-                    <button @click.prevent="deleteFileEntry" class="btn btn-lg btn-danger">Delete</button>
-                </form>
+                <div class="col-md-3">
+                    <h1>files submitted</h1>
+                    <form>
+                        <div class="form-group">
+                            <select v-model="submittedSelectedId" class="form-control" size="7" required>
+                                <option v-for="file in submittedFiles" :value="file.id">
+                                    {{ file.file_name }}
+                                </option>
+                                <option v-if="submittedFiles.length == 0" disabled>no videos submitted</option>
+                            </select>
+                        </div>
+                        <router-link :to="{ name: 'form.edit', params: { id : this.submittedSelectedId } }" class="btn btn-lg btn-primary">Edit</router-link>
+                        <button @click.prevent="deleteFileEntry" class="btn btn-lg btn-danger">Delete</button>
+                    </form>
+                </div>
             </div>
+        </div>
+        <div v-else>
+            <form-login></form-login>
         </div>
     </div>
 </template>
@@ -52,6 +57,7 @@
     export default {
         data() {
             return {
+                user: null,
                 files: [],
                 errors: {},
                 selected: '',
@@ -117,7 +123,7 @@
                 });
             },
             refresh : function() {
-                this.user();
+                // this.user();
                 this.getFiles();
                 this.getSubmittedFiles();
             },
