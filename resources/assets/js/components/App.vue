823446a682766f150c2904d8cc5458c92a2c382b
mike <mike@hipstercreative.com>
2017-02-10 16:03:50 -0800

    Follow up to component cleanup

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <form-nav></form-nav><router-view></router-view>
+        <form-nav></form-nav>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    import components from '../components'
     export default {
-        components: components
+        //
     }
 </script>
